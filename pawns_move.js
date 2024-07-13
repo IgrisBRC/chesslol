6ad604160fdde024151018195190db0248cce216
IgrisBRC <Igris4884@gmail.com>
2024-07-13 10:58:42 +0530

    removed extra pawn move

--- pawns_move.js
+++
@@ -1,7 +0,0 @@
-function pawn_move(board, y, x){
-    let moves =[];    
-    // * if pawn at row 1 or at row 6 it has to choice
-    if(x<board.length && x >-1 && y<board.length && y>-1){
-        moves.push()
-    }
-}
