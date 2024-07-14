93041c88d3e3284ce90321563500956eb635eb71
ame <ameliasquires@disroot.org>
2024-07-14 02:19:12 -0500

    remove semi-colons lol

--- public/sync_board.js
+++ public/sync_board.js
@@ -3,13 +3,13 @@ function sync_board(){
 
     for(let i = 0; i < 8; i++){
         for(let j = 0; j < 8; j++){
-            if(board[i][j] == 0) continue;
+            if(board[i][j] == 0) continue
 
             let square = document.getElementById(`${alphabet.charAt(j)}${8 - i}`)
             let piece = document.createElement('p')
 
             piece.classList.add(board[i][j] > 0 ? 'w' : 'b')
-            square.append(piece);
+            square.append(piece)
             piece.append(table[Math.abs(board[i][j])])
         }
     }
